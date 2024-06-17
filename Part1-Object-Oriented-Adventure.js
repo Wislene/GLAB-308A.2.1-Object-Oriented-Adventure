26787ca02fafb803d9136a289a60ea7228ccbb9f
Wislene <168476494+Wislene@users.noreply.github.com>
2024-06-17 15:23:38 -0400

    update

--- Part1-Object-Oriented-Adventure.js
+++ Part1-Object-Oriented-Adventure.js
@@ -1,30 +1,49 @@
+// const adventurer = {
+//   name: "Robin",
+//   health: 10,
+//   inventory: ["sword", "potion", "artifact"],
+//   companion: {
+//       name: "Leo",
+//       type: "Cat",
 
-const adventurer = {
-  name: "Robin",
-  health: 10,
-  inventory: ["sword", "potion", "artifact"],
-  companion: {
-      name: "Leo",
-      type: "Cat",
+//       companion: {
+//         name: "Frank",
+//         type: "Flea",
+//         belongings: ["small hat", "sunglasses"]
+//       }
+//   },
+//   roll (mod = 0) {
+//     const result = Math.floor(Math.random() * 20) + 1 + mod;
+//     console.log(`${this.name} rolled a ${result}.`)
+//     }
+//   }
 
-      companion: {
-        name: "Frank",
-        type: "Flea",
-        belongings: ["small hat", "sunglasses"]
-      }
-  },
-  roll (mod = 0) {
-    const result = Math.floor(Math.random() * 20) + 1 + mod;
-    console.log(`${this.name} rolled a ${result}.`)
-    }
+//   for (let i=0; i < adventurer.inventory.length; i++) {
+//     console.log(adventurer.inventory[i]);
+//   }
+
+// adventurer.roll();
+
+// Part-B
+
+class Character {
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    this.inventory = [];
   }
- 
-  for (let i=0; i < adventurer.inventory.length; i++) {
-    console.log(adventurer.inventory[i]);
-  }
+  sayName = function () {
+    return "My Name is ${this.name}";
+  };
+}
 
-  
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
 
-// };
-adventurer.roll();
-  
+const fistPerson = new Person("Robin", "Leo", "Frank");
+console.log(name.sayName());
