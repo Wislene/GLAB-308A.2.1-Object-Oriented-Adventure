385ce44a65505150842a97eddfb09d541f3dd2fb
Wislene <168476494+Wislene@users.noreply.github.com>
2024-06-17 14:18:44 -0400

    firstcommit

---
+++ Part1-Object-Oriented-Adventure.js
@@ -0,0 +1,20 @@
+const adventurer = {
+  name: "Robin",
+  health: 10,
+  inventory: ["sword", "potion", "artifact"],
+  companion: {
+      name: "Leo",
+      type: "Cat"
+  }
+  }
+
+  for (let i=0; i , adventurer.inventory.length; i++) {
+    console.log(adventurer.inventory[i]);
+
+  companion: {
+    name: "Frank",
+    type: "Flea",
+    belongings: ["small hat", "sunglasses"]
+
+  }
+}
