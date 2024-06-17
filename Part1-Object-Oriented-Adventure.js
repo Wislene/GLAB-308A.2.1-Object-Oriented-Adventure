cf285c365e637b3bf3c54e984a40d59fee07fdc8
Wislene <168476494+Wislene@users.noreply.github.com>
2024-06-17 14:52:39 -0400

    update

--- Part1-Object-Oriented-Adventure.js
+++ Part1-Object-Oriented-Adventure.js
@@ -1,20 +1,30 @@
+
 const adventurer = {
   name: "Robin",
   health: 10,
   inventory: ["sword", "potion", "artifact"],
   companion: {
       name: "Leo",
-      type: "Cat"
-  }
-  }
+      type: "Cat",
 
-  for (let i=0; i , adventurer.inventory.length; i++) {
+      companion: {
+        name: "Frank",
+        type: "Flea",
+        belongings: ["small hat", "sunglasses"]
+      }
+  },
+  roll (mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}.`)
+    }
+  }
+ 
+  for (let i=0; i < adventurer.inventory.length; i++) {
     console.log(adventurer.inventory[i]);
-
-  companion: {
-    name: "Frank",
-    type: "Flea",
-    belongings: ["small hat", "sunglasses"]
-
   }
-}
+
+  
+
+// };
+adventurer.roll();
+  
